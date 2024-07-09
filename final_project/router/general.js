5245f6232b35e48d2a1e75955a1954d16b9b773a
amymnoyes <amy.yesno@gmail.com>
2024-07-09 13:54:13 -0400

    updated general

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -11,9 +11,20 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+function getList() {
+  return new Promise((resolve, reject) => {
+    resolve(books);
+  });
+}
+
+public_users.get("/", async function (req, res) {
+  try {
+    const listBooks = await getList();
+    res.send(JSON.stringify(listBooks));
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ message: "Internal Server Error" });
+  }
 });
 
 // Get book details based on ISBN
